205386c39e2af2de0c668d10c91f4062c5ad013b
SANG NGUYỄN LÊ <24127231@student.hcmus.edu.vn>
2025-10-17 16:33:03 +0700

    Bai03

---
+++ bai03.py
@@ -0,0 +1,10 @@
+def main():
+
+    a = int(input("Please enter first integer: "))
+    b = int(input("Please enter second integer: "))
+
+    tong = a + b
+    print("Sum: ", tong)
+
+if __name__ == "__main__":
+    main()
