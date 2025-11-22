f1f2580823360279f42b8f1a4e4983d01e47a27d
cpm-201 <chau.pmy@gmail.com>
2025-11-22 10:47:57 +0700

    giao diện hiện thị lúc đề xuất top3

---
+++ frontend/pages/chatBot/chatBot.js
@@ -0,0 +1,13 @@
+/* luồng 1: 
+- chatbot hiển thị kết quả sau lọc, so sánh top3, nên là ở phần giao diện này chỉ cần hiển thị 3 card hotel output
+- 3 card tĩnh
+- hiển thị thông tin ngắn của top3:
+    - tên khách sạn
+    - rating
+    - giới thiệu ngắn
+    - giá cả
+    - địa chỉ
+    - liên hệ
+*/
+
+import anime from 'https://cdn.jsdelivr.net/npm/animejs@3.2.1/lib/anime.es.js';
